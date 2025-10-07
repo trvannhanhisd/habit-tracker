ace53ba18499c653815fc982ad2b1ef29bb5114f
trvannhanh <trvannhanh@gmail.com>
2025-10-07 09:00:14 +0700

    configuration: Clean Architecture, SQRS pattern, mediator pattern

---
+++ HabitTracker/HabitTracker.Application/ConfigureServices.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HabitTracker.Application
+{
+    public static class ConfigureServices
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+        {
+            services.AddAutoMapper(cfg => cfg.AddMaps(Assembly.GetExecutingAssembly()));
+            services.AddMediatR(ctg =>
+            {
+                ctg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
+            });
+
+            return services;
+            
+        }
+    }
+}
