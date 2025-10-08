68a1b2f2970e2f2c61bc2bad48f15708dddc601b
trvannhanh <trvannhanh@gmail.com>
2025-10-08 09:05:51 +0700

    feat: Initial Migrations

---
+++ HabitTracker/HabitTracker.Infrastructure/Migrations/20251007072738_2nd_InitialCreate.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HabitTracker.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class _2nd_InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_habits",
+                table: "habits");
+
+            migrationBuilder.RenameTable(
+                name: "habits",
+                newName: "Habits");
+
+            migrationBuilder.AddColumn<int>(
+                name: "UserId",
+                table: "Habits",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Habits",
+                table: "Habits",
+                column: "Id");
+
+            migrationBuilder.CreateTable(
+                name: "HabitLog",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    HabitId = table.Column<int>(type: "int", nullable: false),
+                    Date = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    IsCompleted = table.Column<bool>(type: "bit", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_HabitLog", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_HabitLog_Habits_HabitId",
+                        column: x => x.HabitId,
+                        principalTable: "Habits",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "User",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_User", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Habits_UserId",
+                table: "Habits",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_HabitLog_HabitId",
+                table: "HabitLog",
+                column: "HabitId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Habits_User_UserId",
+                table: "Habits",
+                column: "UserId",
+                principalTable: "User",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Habits_User_UserId",
+                table: "Habits");
+
+            migrationBuilder.DropTable(
+                name: "HabitLog");
+
+            migrationBuilder.DropTable(
+                name: "User");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Habits",
+                table: "Habits");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Habits_UserId",
+                table: "Habits");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "Habits");
+
+            migrationBuilder.RenameTable(
+                name: "Habits",
+                newName: "habits");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_habits",
+                table: "habits",
+                column: "Id");
+        }
+    }
+}
