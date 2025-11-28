1a62e0cd985d789253b34046ff8a93ad485f6da7
trvannhanh <trvannhanh@gmail.com>
2025-11-28 18:26:57 +0700

    update: Domain Layer Update ()

--- HabitTracker/HabitTracker.Domain/Repository/IHabitRepository.cs
+++ HabitTracker/HabitTracker.Domain/Repository/IHabitRepository.cs
@@ -6,13 +6,13 @@ namespace HabitTracker.Domain.Repository
     {
         IUnitOfWork UnitOfWork { get; }
         Task<List<Habit>> GetAllHabitAsync();
-        Task<Habit?> GetHabitByIdAsync(int habitId);
-        Task<Habit> CreateHabitAsync(Habit habit);
+        Task<Habit> GetHabitByIdAsync(int habitId, CancellationToken cancellationToken = default);
+        Task<Habit> CreateHabitAsync(Habit habit, CancellationToken cancellationToken = default);
         Task<List<Habit>> GetAllHabitsByUserIdAsync(int userId);
         Task<Habit?> GetHabitByUserIdAsync(int userId, int habitId);
         Task<List<Habit>> GetHabitsWithoutLogForDateAsync(DateTime date);
         Task DeleteHabitAsync(int id);
         Task ArchiveHabitAsync(int habitId);
-        Task UpdateHabitAsync(Habit habit);
+        Task UpdateHabitAsync(Habit habit, CancellationToken cancellationToken = default);
     }
 }
